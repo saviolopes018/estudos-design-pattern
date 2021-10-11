39639a853e57c7820cf6339fb7dcd7158897ff29
Sávio Lopes <savio.lopes404@gmail.com>
2021-10-11 08:05:45 -0300

    stategy

---
+++ loja/src/br/com/loja/TestesImpostos.java
@@ -0,0 +1,17 @@
+package br.com.loja;
+
+import java.math.BigDecimal;
+
+import br.com.loja.imposto.CalculadoraDeImpostos;
+import br.com.loja.imposto.ICMS;
+import br.com.loja.orcamento.Orcamento;
+
+public class TestesImpostos {
+
+	public static void main(String[] args) {
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+		System.out.println(calculadora.calcular(orcamento, new ICMS()));
+	}
+
+}
