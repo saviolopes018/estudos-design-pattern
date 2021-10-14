5eec9485958f258518673b8c883cb42def24930c
Sávio Lopes <savio.lopes404@gmail.com>
2021-10-13 22:46:56 -0300

    State

--- loja/src/br/com/loja/orcamento/Orcamento.java
+++ loja/src/br/com/loja/orcamento/Orcamento.java
@@ -2,15 +2,37 @@ package br.com.loja.orcamento;
 
 import java.math.BigDecimal;
 
+import br.com.loja.orcamento.situacao.EmAnalise;
+import br.com.loja.orcamento.situacao.SituacaoOrcamento;
+
 public class Orcamento {
 
 	private BigDecimal valor;
 	private Long quantidadeItens;
+	private SituacaoOrcamento situacao;
 	
 	public Orcamento(BigDecimal valor, Long quantidadeItens) {
 		super();
 		this.valor = valor;
 		this.quantidadeItens = quantidadeItens;
+		this.situacao = new EmAnalise();
+	}
+	
+	public void aplicarDescontoExtra() {
+		BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);	
+		this.valor = this.valor.subtract(valorDoDescontoExtra);
+	}
+	
+	public void aprovar() {
+		this.situacao.aprovar(this);
+	}
+	
+	public void reprovar() {
+		this.situacao.reprovar(this);
+	}
+	
+	public void finalizar() {
+		this.situacao.finalizar(this);
 	}
 
 	public BigDecimal getValor() {
@@ -19,6 +41,14 @@ public class Orcamento {
 
 	public Long getQuantidadeItens() {
 		return quantidadeItens;
+	}
+
+	public SituacaoOrcamento getSituacao() {
+		return situacao;
+	}
+	
+	public void setSituacao(SituacaoOrcamento situacao) {
+		this.situacao = situacao;
 	}
 	
 }
---
+++ loja/src/br/com/loja/orcamento/situacao/Aprovado.java
@@ -0,0 +1,17 @@
+package br.com.loja.orcamento.situacao;
+
+import java.math.BigDecimal;
+
+import br.com.loja.orcamento.Orcamento;
+
+public class Aprovado extends SituacaoOrcamento{
+	
+	public BigDecimal calcularValorDescontoExtra(Orcamento orcamento) {
+		return orcamento.getValor().multiply(new BigDecimal("0.02"));
+	}
+	
+	public void finalizar(Orcamento orcamento) {
+		orcamento.setSituacao(new Finalizado());
+	}
+
+}
