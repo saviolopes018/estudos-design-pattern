81c7bbd09fd59af32cb04efe80d348450e83b0f6
Sávio Lopes <savio.lopes404@gmail.com>
2021-10-14 22:43:19 -0300

    Command

---
+++ loja/src/br/com/loja/pedido/GeraPedido.java
@@ -0,0 +1,31 @@
+package br.com.loja.pedido;
+
+import java.math.BigDecimal;
+
+public class GeraPedido {
+
+	private String cliente;
+	private BigDecimal valorOrcamento;
+	private Long quantidadeItens;
+	
+	public GeraPedido(String cliente, BigDecimal valorOrcamento, Long quantidadeItens) {
+		super();
+		this.cliente = cliente;
+		this.valorOrcamento = valorOrcamento;
+		this.quantidadeItens = quantidadeItens;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public BigDecimal getValorOrcamento() {
+		return valorOrcamento;
+	}
+
+	public Long getQuantidadeItens() {
+		return quantidadeItens;
+	}
+	
+	
+}
---
+++ loja/src/br/com/loja/pedido/Pedido.java
@@ -0,0 +1,32 @@
+package br.com.loja.pedido;
+
+import java.time.LocalDateTime;
+
+import br.com.loja.orcamento.Orcamento;
+
+public class Pedido {
+	
+	private String cliente;
+	private LocalDateTime data;
+	private Orcamento orcamento;
+	
+	public Pedido(String cliente, LocalDateTime data, Orcamento orcamento) {
+		super();
+		this.cliente = cliente;
+		this.data = data;
+		this.orcamento = orcamento;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public Orcamento getOrcamento() {
+		return orcamento;
+	}
+		
+}
