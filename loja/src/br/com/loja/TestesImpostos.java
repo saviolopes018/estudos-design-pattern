54c763ab89b4d86006a182fe0a838e795b5f3435
Sávio Lopes <savio.lopes404@gmail.com>
2021-10-16 00:08:36 -0300

    Decorators

--- loja/src/br/com/loja/TestesImpostos.java
+++ loja/src/br/com/loja/TestesImpostos.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.loja.imposto.CalculadoraDeImpostos;
 import br.com.loja.imposto.ICMS;
+import br.com.loja.imposto.ISS;
 import br.com.loja.orcamento.Orcamento;
 
 public class TestesImpostos {
@@ -11,7 +12,7 @@ public class TestesImpostos {
 	public static void main(String[] args) {
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1L);
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-		System.out.println(calculadora.calcular(orcamento, new ICMS()));
+		System.out.println(calculadora.calcular(orcamento, new ISS(new ICMS(null))));
 	}
 
 }
--- loja/src/br/com/loja/imposto/Imposto.java
+++ loja/src/br/com/loja/imposto/Imposto.java
@@ -4,8 +4,23 @@ import java.math.BigDecimal;
 
 import br.com.loja.orcamento.Orcamento;
 
-public interface Imposto {
+public abstract class Imposto {
 	
-	BigDecimal calcular(Orcamento orcamento);
+	private Imposto imposto;
+	
+	public Imposto(Imposto imposto) {
+		this.imposto = imposto;
+	}
+	
+	protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
+	
+	public BigDecimal calcular(Orcamento orcamento) {
+		BigDecimal valorImpostoPrimario = realizarCalculo(orcamento);
+		BigDecimal valorImpostoSecundario = BigDecimal.ZERO;
+		if(imposto != null) {
+			valorImpostoSecundario = imposto.realizarCalculo(orcamento);
+		}
+		return valorImpostoPrimario.add(valorImpostoSecundario);
+	}
 
 }
